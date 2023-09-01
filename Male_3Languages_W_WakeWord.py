11d4f1b03b1c2eff002d1cd2a743a7136f3c5b01
Trina <trina.shih@gmail.com>
2023-08-31 22:54:52 -0700

    Update Male_3Languages_W_WakeWord.py
    Fix wakeword multi-entry issue and clear the queue after wakeword is detected.

--- Male_3Languages_W_WakeWord.py
+++ Male_3Languages_W_WakeWord.py
@@ -2,6 +2,7 @@
 # Trina S. Modified a script from vosk example. 
 # prerequisites: as described in https://alphacephei.com/vosk/install and also python module `sounddevice` (simply run command `pip install sounddevice`)
 # It's only for Voice Hat V1. Microphone is at device 1.
+# wakeword = "computer"
 
 
 import os
@@ -30,7 +31,7 @@ import sounddevice as sd
 from vosk import Model, KaldiRecognizer
 
 q = queue.Queue()
-wake_word = "您好 优雅"
+wake_word = "computer"
 
 def int_or_str(text):
     """Helper function for argument parsing."""
@@ -38,6 +39,18 @@ def int_or_str(text):
         return int(text)
     except ValueError:
         return text
+
+cooldown_time = 2  # seconds
+cooldown_timestamp = 0
+
+def can_detect_wake_word(current_time):
+    global cooldown_timestamp
+    global cooldown_time
+    if current_time - cooldown_timestamp > cooldown_time:
+        cooldown_timestamp = current_time
+        return True
+    return False
+
 
 def callback(indata, frames, time, status):
     """This is called (from a separate thread) for each audio block."""
@@ -59,7 +72,7 @@ parser.add_argument(
     help="input device (numeric ID or substring)")
 
 parser.add_argument(
-    "-m", "--model", type=str, default="cn", help="language model; e.g. en-us, fr, nl; default is cn")
+    "-m", "--model", type=str, default="en-us", help="language model; e.g. en-us, fr, nl, cn; default is en-us")
 args = parser.parse_args(remaining)
 
 
@@ -87,7 +100,7 @@ def check_button_press():
 def text_to_speech(input_text, output_filename="output.mp3"):
 
     # Assuming the credentials file is in the same directory
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = " Your text2speech.json"
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "Your text2speech.json"
 
     # Instantiates a client
     client = texttospeech_v1.TextToSpeechClient()
@@ -141,11 +154,6 @@ def play_mp3(filename):
 
     while pygame.mixer.music.get_busy():
         pygame.time.Clock().tick(10)
-
-
-
-
-
 
 
 
@@ -261,39 +269,45 @@ try:
                 else:
                     recognized_text = rec.PartialResult()
                     if wake_word in recognized_text:
-                        print("Wake word detected!")
-                        #break
-                        conversations = []
-                        # role: system, user, assistant
-                        conversations.append({'role': 'system', 'content': 'You are a intelligent assistant.'})
-                        conversations = chatgpt_conversation(conversations)
+                        current_time = time.time()
+                        #prevent multi-entry.
+                        if can_detect_wake_word(current_time):
+                            print("Wake word detected!")
+                            #clean queue q.
+                            while not q.empty():
+                                q.get()
+                            #break
+                            conversations = []
+                            # role: system, user, assistant
+                            conversations.append({'role': 'system', 'content': 'You are a intelligent assistant.'})
+                            conversations = chatgpt_conversation(conversations)
 
-                        print('{0}: {1}\n'.format(conversations[-1]['role'].strip(), conversations[-1]['content'].strip()))
+                            print('{0}: {1}\n'.format(conversations[-1]['role'].strip(), conversations[-1]['content'].strip()))
                         #filename = text_to_speech('{0}: {1}'.format(conversations[-1]['role'].strip(), conversations[-1]['content'].strip()))
                         #print(f"Audio saved to: {filename}")
-                        play_mp3("greeting_TW_M.mp3")
+                            play_mp3("greeting_TW_M.mp3")
 
-                        while not StatusControl:
-                            prompt = record_and_recognize()
-                            if prompt==999:
-                                print("Code exit")
-                                break
-                            conversations.append({'role': 'user', 'content': prompt})
-                            conversations = chatgpt_conversation(conversations)
-                            print()
-                            print('{0}: {1}\n'.format(conversations[-1]['role'].strip(), conversations[-1]['content'].strip()))
-                            #print(conversations[-1]['content'].strip())
+                            while not StatusControl:
+                                prompt = record_and_recognize()
+                                if prompt==999:
+                                    print("Code exit")
+                                    break
+                                conversations.append({'role': 'user', 'content': prompt})
+                                conversations = chatgpt_conversation(conversations)
+                                print()
+                                print('{0}: {1}\n'.format(conversations[-1]['role'].strip(), conversations[-1]['content'].strip()))
+                                #print(conversations[-1]['content'].strip())
 
-                            filename = text_to_speech(conversations[-1]['content'].strip())
-                            print(f"Audio saved to: {filename}")
-                            #with Board() as board:
-                            #    board.led.state = Led.ON
-                            play_mp3(filename)
+                                filename = text_to_speech(conversations[-1]['content'].strip())
+                                print(f"Audio saved to: {filename}")
+                                #with Board() as board:
+                                #    board.led.state = Led.ON
+                                play_mp3(filename)
                 
-                        StatusControl = False
-                        break
-                    else:
-                        print(recognized_text)
+                            StatusControl = False
+                            break
+                        else:
+                            print(recognized_text)
 
             
 
